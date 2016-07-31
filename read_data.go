7026df159507e91abd15385569b5950529895c24
wanglifeng <wanglifeng@yidian-inc.com>
2016-07-31 22:00:14 +0800

    add read data

---
+++ read_data.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"github.com/tealeg/xlsx"
+	"log"
+	"github.com/nguyenthenguyen/docx"
+	"strings"
+	"fmt"
+	"path/filepath"
+)
+
+type XlsxData struct {
+	file_path 	string
+	fh 		*xlsx.File
+}
+
+func NewXlsxData(file_path string) (*XlsxData, error) {
+
+	data_xlsx, err := xlsx.OpenFile(file_path)
+
+	if err != nil {
+		return nil, err
+	} else {
+		return &XlsxData{file_path: file_path, fh: data_xlsx}, nil
+	}
+}
+
+func (xlsx *XlsxData) getSheets() ([]string) {
+
+	var result []string
+
+	sheets := xlsx.fh.Sheets
+	for _, sheet:= range sheets {
+		result = append(result, sheet.Name)
+	}
+
+	return result
+}
+
+func (xlsx *XlsxData) getTitles(sheet_index int) ([]string){
+
+	var result []string
+
+	for i, sheet := range xlsx.fh.Sheets {
+		if i == sheet_index {
+			log.Printf("i=%d", i)
+			for index, row := range sheet.Rows {
+				if index == 0 {
+					for _, col := range row.Cells {
+						title, _ := col.String()
+						result = append(result, title)
+					}
+					return result
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+func (xlsx *XlsxData) replace(sheet_index, name_index int, template, save_path string) {
+
+	template_doc, err := docx.ReadDocxFile(template)
+
+	if err != nil {
+		log.Fatalf("open template file with err: %v", err)
+	}
+	defer template_doc.Close()
+
+	titles := make(map[int] string)
+
+	for index, sheet := range xlsx.fh.Sheets {
+		if index == sheet_index {
+
+			for r, row := range sheet.Rows {
+				if r== 0 {
+					for i, col := range row.Cells {
+						titles[i], _ = col.String()
+						titles[i] = strings.TrimSpace(titles[i])
+						fmt.Printf("title: %d:\t%s\n", i, titles[i])
+					}
+				} else {
+					new_doc := template_doc.Editable()
+
+					file_name := ""
+
+					for i, col := range row.Cells {
+						value, _ := col.String()
+
+						if i == name_index {
+							file_name = value
+						}
+
+						fmt.Printf("replace %s:\t%s\n", titles[i], value)
+						new_doc.Replace(titles[i], value, -1)
+					}
+
+					new_path := fmt.Sprintf("%s/%s.docx", filepath.ToSlash(save_path), file_name)
+					log.Printf("write new file %s", filepath.FromSlash(new_path))
+
+					new_doc.WriteToFile(new_path)
+				}
+			}
+		}
+	}
+}
